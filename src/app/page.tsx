2a5377694efe8a838ad140084e1e2369f7298893
Otto Hines <ottohines1@gmail.com>
2025-05-16 15:08:42 -0500

    added more styling to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 
 export default function Home() {
   return (
-    <div className="mx-auto my-auto">
-      <h1>Hi Welcome to my domain! </h1>
+
+
+    <div className="flex flex-col m-20 ">
+      <h1 className="text-4xl mb-5 font-bold ">Software Developer, Trader, and <br />creative builder.</h1>
+      <p className="text-[#8f8f99]">I’m Otto, a full-stack developer and trader with a background in economics,
+        currently based in Chicago. I build clean, high-impact web applications and develop trading systems that
+        blend structure with intuition. Whether I’m refining an options model or designing a user experience,
+        I care about clarity, performance, and results.</p>
     </div>
+
   );
 }
